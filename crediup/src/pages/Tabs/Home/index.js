34ae4281e1e3e39845657564e4b0af48bfd7be18
Emerson Silva <emerson_jdss@hotmail.com>
2019-06-08 20:27:02 -0300

    changes on home screen

--- crediup/src/pages/Tabs/Home/index.js
+++ crediup/src/pages/Tabs/Home/index.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Text, View, Image } from 'react-native';
 import { Divider } from 'react-native-elements';
-import { Container, CardArea } from './styles';
+import {
+  Container,
+  CardArea,
+  ContainerUserData,
+  LeftArea,
+  RightArea,
+  GenerateCard,
+} from './styles';
 import card from '../../../assets/img/c_user.png';
 
 export class Home extends Component {
@@ -18,6 +25,25 @@ export class Home extends Component {
           <Image source={card} resizeMode="contain" style={{ width: '100%', height: '100%' }} />
         </CardArea>
         <Divider style={{ backgroundColor: 'blue' }} />
+
+        <ContainerUserData>
+          <LeftArea>
+            <Text style={{ color: 'blue' }}>Limite Disponivel / Ocultar</Text>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ color: 'blue' }}>R$</Text>
+
+              <Text style={{ fontSize: 28, color: 'blue' }}> 800,00 </Text>
+            </View>
+          </LeftArea>
+          <RightArea>
+            <GenerateCard>
+              <Text style={{ color: 'blue' }}> Gerar </Text>
+              <Text style={{ color: 'blue' }}>Cartao Virtual</Text>
+            </GenerateCard>
+          </RightArea>
+        </ContainerUserData>
+        <Divider style={{ backgroundColor: 'blue' }} />
+        <ContainerUserData />
       </Container>
     );
   }
--- crediup/src/pages/Tabs/Home/styles.js
+++ crediup/src/pages/Tabs/Home/styles.js
@@ -15,3 +15,37 @@ export const CardArea = styled.View`
   justify-content: center;
   margin-bottom: 20px;
 `;
+
+export const ContainerUserData = styled.View`
+  height: 80;
+  width: 100%;
+  background-color: yellow;
+  flex-direction: row;
+  margin-bottom: ${metrics.basePadding}px;
+  margin-top: ${metrics.basePadding}px;
+`;
+export const LeftArea = styled.View`
+  height: 100%;
+  width: 60%;
+  /* background-color: orange; */
+  padding: 15px;
+`;
+
+export const RightArea = styled.View`
+  height: 100%;
+  width: 40%;
+  /* background-color: lightgray; */
+  align-items: center;
+  justify-content: center;
+`;
+
+export const GenerateCard = styled.View`
+  height: 80%;
+  width: 80%;
+  align-self: center;
+  border-width: 2px;
+  border-color: blue;
+  border-radius: ${metrics.baseRadius};
+  align-items: center;
+  justify-content: center;
+`;
